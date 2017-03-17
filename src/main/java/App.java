f353f83a76fcb687877ff55cb0ca0ce7a74191fd
Blake Womack <lakebayomackway@gmail.com>
2017-03-17 15:22:13 -0700

    add coupon to App.java.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -24,13 +24,30 @@ public class App {
     System.out.println("1: Carl the Comedian, 2: Chamber Music, 3: Rock n Roll, 4: Jazz, 5: Marty the Mitzvah MC 6: None");
     String entInput = myConsole.readLine();
     int ent = Integer.parseInt(entInput);
-    System.out.println("Question 5: Are you ready to calculate your estimate?");
+//add coupon price reduction message here
+    System.out.println("Question 5: Do you have a coupon?");
+    System.out.println("Please respond with the appropriate NUMBER");
+    System.out.println("1: Yes, 2: No");
+    String couponInput = myConsole.readLine();
+    int coupon = Integer.parseInt(couponInput);
+    System.out.println("Enter your coupon number!");
+    String couponNumberInput = myConsole.readLine();
+    int couponNumber = Integer.parseInt(couponNumberInput);
+    System.out.println("Question 6: Are you ready to calculate your estimate?");
     System.out.println("Please respond with the appropriate NUMBER");
     System.out.println("1: Yes, 2: No");
     String calculateNowInput = myConsole.readLine();
+
+//install coupon logic in front end
     int calculateNow = Integer.parseInt(calculateNowInput);
     Integer estimateResult = event.initialEstimate(calculateNow, event.isEvent(guests), event.typeOfFood(type), event.extraForAlcohol(bar), event.extraForEntertainment(ent));
-    System.out.println("The price for your party is: $" + estimateResult);
+      if (coupon == 1) {
+        Float newResult = estimateResult \ 10;
+        System.out.println("The price for your party is: $" + newResult);
+        System.out.println("You saved 10 percent");
+      } else {
+        System.out.println("The price for your party is: $" + estimateResult);
+    }
   }
 
 }
