25ceedaa8dbd611d13d24b4a4467720985dfe84f
likewater <lakebayomackway@gmail.com>
2017-03-21 20:22:39 -0700

    add getters to EventTest.

--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -3,38 +3,38 @@ import static org.junit.Assert.*;
 
 public class EventTest {
   @Test
-  public void isEvent_forNumberOfGuests_16() {
+  public void getIsEvent_forNumberOfGuests_16() {
     Event event = new Event();
     Integer cost = 16;
-    assertEquals(cost, event.isEvent(1));
+    assertEquals(cost, event.getIsEvent(1));
    }
 
    @Test
-   public void typeOfFood_extraPerHeadForTypeOfFood_5() {
+   public void getTypeOfFood_extraPerHeadForTypeOfFood_5() {
      Event event = new Event();
      Integer costOfFood = 5;
-     assertEquals(costOfFood, event.typeOfFood(4));
+     assertEquals(costOfFood, event.getTypeOfFood(4));
     }
 
     @Test
-    public void fullBar_extraPerHeadForAlcohol_10() {
+    public void getExtraForAlcohol_extraPerHeadForAlcohol_10() {
       Event event = new Event();
       Integer costOfAlcohol = 10;
-      assertEquals(costOfAlcohol, event.extraForAlcohol(1));
+      assertEquals(costOfAlcohol, event.getExtraForAlcohol(1));
     }
 
     @Test
-    public void typeOfEntertainment_extraForTypeOfEntertainment_300() {
+    public void getExtraForEntertainment_extraForTypeOfEntertainment_300() {
       Event event = new Event();
       Integer costOfEntertainment = 300;
-      assertEquals(costOfEntertainment, event.extraForEntertainment(1));
+      assertEquals(costOfEntertainment, event.getExtraForEntertainment(1));
     }
 
     @Test
-    public void initialEstimate_addCostsOfAllChoices_331() {
+    public void getInitialEstimate_addCostsOfAllChoices_331() {
       Event event = new Event();
       Integer initialEstimate = 331;
-      assertEquals(initialEstimate, event.initialEstimate(1, 16, 10, 5, 300));
+      assertEquals(initialEstimate, event.getInitialEstimate(1, 16, 10, 5, 300));
     }
 
 }
